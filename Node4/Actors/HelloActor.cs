9dcf0a8f2ae7384a33a23185f6afc7212cea03b8
johnny <johnny@wantgoo.com>
2019-05-03 15:41:17 +0800

    Cluster Router

---
+++ Node4/Actors/HelloActor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Akka.Actor;
+
+namespace Node4.Actors
+{
+    public class HelloActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            Console.WriteLine($"{this.Self.Path}: Hello");
+        }
+    }
+}
