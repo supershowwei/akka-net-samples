89fb38e552b1208aa90b5465d6167f8cbc490449
johnny <johnny@wantgoo.com>
2019-07-03 12:20:08 +0800

    add BecomeLab

---
+++ BecomeLab/Actors/TestActor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Akka;
+using Akka.Actor;
+
+namespace BecomeLab.Actors
+{
+    public class TestActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            this.Abc(message);
+        }
+
+        private void Abc(object message)
+        {
+            message.Match().With<int>(_ =>
+                {
+                    Console.WriteLine(_);
+
+                    this.Become(this.Def);
+                });
+        }
+
+        private void Def(object message)
+        {
+            message.Match().With<string>(_ =>
+                {
+                    Console.WriteLine(_);
+
+                    if (_ == "123")
+                    {
+                        var a = 1;
+                        var b = 0;
+                        var c = a / b;
+                    }
+                });
+        }
+    }
+}
---
+++ BecomeLab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Akka.Actor;
+using BecomeLab.Actors;
+
+namespace BecomeLab
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sys = ActorSystem.Create("sys");
+
+            var actor = sys.ActorOf(Props.Create<TestActor>());
+
+            actor.Tell(1);
+
+            Console.ReadKey();
+
+            actor.Tell("111");
+
+            Console.ReadKey();
+
+            actor.Tell("123");
+
+            Console.ReadKey();
+
+            actor.Tell("111");
+            actor.Tell(1);
+
+            Console.ReadKey();
+        }
+    }
+}
